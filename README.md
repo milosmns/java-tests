7221de642c1c251162c20ffb3b736a916a049577
Milos Marinkovic <milosmns@gmail.com>
2015-09-26 18:27:50 +0200

    Adding the initial for loop test Adding gitignore Adding a README

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# java-tests
+# Java Tests
+
+Description
+-----------
+
 A simple repository with source code for my Java and JVM tests
+
+Contributions
+-------------
+
+Feel free to add your own tests if you have some good ideas
---
+++ src/loops/forl/ForLoopTest.java
@@ -0,0 +1,63 @@
+package loops.forl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ForLoopTest {
+
+    public static void main(String[] args) {
+        ArrayList<String> ls = new ArrayList<>();
+
+        for (int j = 0; j < 1000000; j++) {
+            ls.add("one");
+            ls.add("two");
+            ls.add("three");
+            ls.add("four");
+            ls.add("five");
+        }
+
+        long startTime1 = System.nanoTime();
+        int count1 = loop1(ls);
+        long elapsed1 = System.nanoTime() - startTime1;
+
+        long startTime2 = System.nanoTime();
+        int count2 = loop2(ls);
+        long elapsed2 = System.nanoTime() - startTime2;
+
+        long startTime3 = System.nanoTime();
+        int count3 = loop3(ls);
+        long elapsed3 = System.nanoTime() - startTime3;
+
+        System.out.println("Elapsed1: " + elapsed1 + " ns, item count " + count1);
+        System.out.println("Elapsed2: " + elapsed2 + " ns, item count " + count2);
+        System.out.println("Elapsed3: " + elapsed3 + " ns, item count " + count3);
+    }
+
+    private static int loop1(List<String> list) {
+        int charCount = 0;
+        // noinspection ForLoopReplaceableByForEach
+        for (int i = 0; i < list.size(); i++) {
+            charCount += list.get(i).length();
+        }
+        return charCount;
+    }
+
+    private static int loop2(List<String> list) {
+        int charCount = 0;
+        int listCount = list.size();
+        // noinspection ForLoopReplaceableByForEach
+        for (int i = 0; i < listCount; i++) {
+            charCount += list.get(i).length();
+        }
+        return charCount;
+    }
+
+    private static int loop3(List<String> list) {
+        int charCount = 0;
+        for (String s : list) {
+            charCount += s.length();
+        }
+        return charCount;
+    }
+
+}
