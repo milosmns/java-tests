6592ce9f63870fb2b09546dab32ef2a4d28fb99f
Milos Marinkovic <milosmns@gmail.com>
2015-10-05 18:29:22 +0200

    Add class comparison test stub

---
+++ src/comparison/ClassComparisonTest.java
@@ -0,0 +1,12 @@
+package comparison;
+
+/**
+ * A comparison test for object classes. Iterating through 2 custom class datasets, counting how many equal values are there for each
+ * comparison type. Comparison methods include class to class (by instance), class to class (using {@link Object#equals(Object)}, class to
+ * class (by name comparison), using the {@code instanceof} operator, using {@link Class#isAssignableFrom(Class)}, etc.
+ */
+public class ClassComparisonTest {
+
+    // don't forget to use Object.class.isAssignableFrom(type)
+
+}
